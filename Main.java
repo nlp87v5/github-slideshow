25b9d700fe31ebcb8659f307630b6cab7913ea53
nlp87v5 <67182067+nlp87v5@users.noreply.github.com>
2020-06-20 14:35:21 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javafx.scene.effect.Blend;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * @author 宁临鹏
+ * @date 2020/6/19 - 2020.6.19
+ */
+public class Student {
+    //姓名
+    String name;
+    //年龄
+    int age;
+    //地址
+    String address；
+
+    //定义方法
+    public void study(){
+        Sysetm.out.println("学生热爱学习");
+    } ·
+
+    //吃饭的方法
+    public void eat(){
+        System.out.println("学习饿了要吃饭！");
+    }
+
+    //睡觉的方法
+    public void sleep(){
+        System.out.println("学习累了要睡觉！");
+    }
+}
